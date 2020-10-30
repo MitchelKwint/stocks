d24ded887ff9c9bb28fdbb70f67ddc54a9969627
Mitchel <Contactmitchyt@gmail.com>
2020-10-30 18:14:35 +0100

    commit

--- investing.py
+++ investing.py
@@ -1,3 +1,16 @@
-import matplotlib
-import numpy
-import sklearn
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+
+clf = svm.SVC(gamma=0.0001, C=100)
+
+x,y = digits.data[:-10], digits.target[:-10]
+clf.fit(x,y)
+
+print('Prediction:', clf.predict(digits.data[-9:]))
+
+plt.imshow(digits.images[-9], cmap='gray', interpolation='nearest')
+plt.show()
