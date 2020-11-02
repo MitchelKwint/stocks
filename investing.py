42beabb8dc4d6f03b1f20c315ba68249456d377b
Mitchel <Contactmitchyt@gmail.com>
2020-11-02 15:34:48 +0100

    commit

--- investing.py
+++ investing.py
@@ -1,16 +1,57 @@
-import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import time
+from datetime import datetime
 
-from sklearn import datasets
-from sklearn import svm
+path = "C:/intraQuarter"
 
-digits = datasets.load_digits()
+def Key_Stats(gather="Total Debt/Equity (mrq)"):
+    statspath = path+'/_KeyStats'
+    stock_list = [x[0] for x in os.walk(statspath)]
+    df = pd.DataFrame (columns = ['Date','Unix','Ticker','DE Ratio','Price','sp500'])
 
-clf = svm.SVC(gamma=0.0001, C=100)
+    sp500_df = pd.read_csv('YAHOO-INDEX_GSPC.csv')
+    #print(sp500_df)
 
-x,y = digits.data[:-10], digits.target[:-10]
-clf.fit(x,y)
+    for each_dir in stock_list[1:25]:
+        each_file = os.listdir (each_dir)
+        ticker = each_dir.split("\\")[1]
+        if len(each_file) > 0:
+            for file in each_file:
+                date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
+                unix_time = time.mktime(date_stamp.timetuple())
+                full_file_path = each_dir+'/'+file
+                source = open(full_file_path, 'r').read()
+                try:
+                    value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    
+                    try:
+                        sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
+                        row = sp500_df[(sp500_df['Date'] == sp500_date)]
+                        sp500_value = float(row['Adj Close'])
+                    except:
+                        sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
+                        row = sp500_df[(sp500_df['Date'] == sp500_date)]
+                        sp500_value = float(row['Adj Close'])
 
-print('Prediction:', clf.predict(digits.data[-9:]))
 
-plt.imshow(digits.images[-9], cmap='gray', interpolation='nearest')
-plt.show()
+                    stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+                    #print("stock_price:",stock_price,"Ticker:",ticker)
+
+
+                    df = df.append({'Date':date_stamp,
+                                    'Unix':unix_time,
+                                    'Ticker':ticker,
+                                    'DE Ratio':value,
+                                    'Price':stock_price,
+                                    'sp500':sp500_value,}, ignore_index = True)
+                except Exception as e:
+                    pass
+
+    save = gather.replace(' ','').replace(')','').replace('(','').replace('/','') + ('.csv')
+    print(save)
+    df.to_csv(save)
+
+
+
+Key_Stats() 
